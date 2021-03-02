5e74fbb2e84b6aae0257a7265e17482b13430211
Amin Mansouri <49798286+amansouri3476@users.noreply.github.com>
2021-03-02 21:21:38 +0330

    Add files via upload

---
+++ Assignment 1/Programming/Problem 3/relu-2h/main_glorot.py
@@ -0,0 +1,23 @@
+import numpy as np
+import torch
+import torchvision
+import pickle
+
+from solution import load_cifar10
+data = load_cifar10('/tmp/data', flatten=True)  # can use flatten=False to get the image shape.
+train_data, valid_data, test_data = data
+image, label = train_data[0][0], train_data[1][0]
+image.shape
+
+from solution import NN
+import time
+nn = NN(data=data)
+start = time.time()
+logs = nn.train_loop(20)
+print(time.time()-start)
+print(nn.evaluate())
+
+with open('logs_glorot_relu_2h.pickle','wb') as f:
+    pickle.dump(logs,f)
+
+
